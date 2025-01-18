60e770ca4654ee2d6d56344a3065c57ed5ef664b
Lea Simone <apple@apples-MacBook-Pro.local>
2025-01-17 19:17:52 -0600

    first commit

--- practicalloops.js
+++ practicalloops.js
@@ -30,26 +30,30 @@ for (let f = 1; f <= 100; f++)
 // As soon as you find the prime number, log that number and exit the loop
 
 // User inputs a random number that is less than 100
-let n = 10;
 // The lopp is checking the next number to see whether or not it's prime
 // Create a variable to represent the following number (by adding 1 to the original)
-for (let p = n + 1; p <= 100; p++) {
-    let isPrime = true;
 
-    for (let d = 2; d < p; d++){
+let num = 3;
+let limit = 50; 
+let nextPrime = num+1
 
-        if (p % d === 0) {
-            isPrime = false;
-        }
-    }
+while(num<=limit){
+    let isPrime = true
+    for (let i=2; i<=nextPrime; i++){
+        // checks every number between 2 and 7
+        if(nextPrime%i ===0){
+            isPrime = false
+            console.log(`Your ${num} is not prime`);
+            break
+        } 
+     }
 
-    if (isPrime) {
-        console.log(`Number: ${n}`)
-        console.log(`Next prime number: ${p}`)
+     if (isPrime){
+        console.log(num);
         break;
+        }
+        num++
     }
-
-}
 
 // Part 3: Feeling Loopy
 
@@ -61,58 +65,44 @@ for (let p = n + 1; p <= 100; p++) {
     // There will only be 4 cells per row.
     // There will be no escaped characters other than “\n”.
 
-const csvString ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+//let csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+let csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
 
-// Create an array to store rows
-let rows = [];
-// Create variable with an empty string
-let currentRow = "";
+let cell1= ""
+let cell2= ""
+let cell3= ""
+let cell4= ""
+let currentCell = 1
 
-// Separate csvString into rows
-for (let i = 0; i < csvString.length; i++) {
-    if (csvString[i] === '\n') { // End of the row
-        rows[rows.length] = currentRow; // Puts the finished row in the array
-        currentRow = ""; // Beginning for new row
-    } else {
-        currentRow += csvString[i]; // Each character goes into the variable one by one per loop
+for ( let i=0; i<=csv.length; i++){
+    // this will log each letter individually on a different line
+    //console.log(csv[i]);
+    let char =csv[i];
+    //console.log(char);
+    if(char===","){
+        currentCell++ 
+    } else if (char==="\n"|| i ===csv.length){
+        console.log(cell1,cell2,cell3,cell4);
+        cell1="";
+        cell2="";
+        cell3="";
+        cell4="";
+        currentCell= 1
+    } else { 
+        if(currentCell ===1){
+            cell1+=char;
+        } else if(currentCell ===2){
+            cell2+=char;
+        }else if(currentCell ===3){
+            cell2+=char;
+        }else if(currentCell ===4){
+            cell2+=char;
+        }
     }
 }
-rows[rows.length] = currentRow; // Adds the last row, which doesn't need with the break
-console.log(rows)
-
-// Create an array to store headers
-let headers = [];
-// Create variable wirh an empty string
-let currentHeader = "";
-
-// Extract headers
-for (let i = 0; i < rows[0].length; i++) {
-    if (rows[0][i] === ",") { // End of the header
-        headers[headers.length] = currentHeader; // Puts the finished header in the array
-        currentHeader = ""; // Beginning for new header
-    } else {
-        currentHeader += rows[0][i];
-    }
-}
-headers[headers.length] = currentHeader;
-
-console.log(headers);
+//console.log(cell1);
+//console.log(cell2);
+//console.log(cell3);
+//console.log(cell4);
 
 
-// const rows = csvString.split("\n"); // Split by line breaks into rows
-// console.log(rows)
-
-// const headers = rows[0].split(","); // Extract headers for each row
-// console.log(headers)
-
-// const info = rows.map(row => {
-//     console.log(row);
-// })
-
-// const tableData = []
-
-// const data = rows.slice(1).map(row => {
-//     const values = row.split(1);
-//     const tableData = {};
-//     headers.forEach((headers, index))
-// })
